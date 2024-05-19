edf85ae61b13c96736072b90837bb2761ec94273
Nipuna Theekshana <124187309+NipunaTheekshanaDeMel@users.noreply.github.com>
2024-05-19 13:05:52 +0530

    add item entity

---
+++ src/main/java/software/nipunatheekshana/shoe_shop_management_system/entity/ItemEntity.java
@@ -0,0 +1,40 @@
+package software.nipunatheekshana.shoe_shop_management_system.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import software.nipunatheekshana.shoe_shop_management_system.entity.enums.Gender;
+import software.nipunatheekshana.shoe_shop_management_system.entity.enums.OccasionType;
+import software.nipunatheekshana.shoe_shop_management_system.entity.enums.VerityType;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "item")
+@Entity
+public class ItemEntity implements SuperEntity{
+    @Id
+    private String itemCode;
+    private String itemDesc;
+    @Column(columnDefinition = "LONGTEXT")
+    private String itemPic;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    @Enumerated(EnumType.STRING)
+    private OccasionType occasionType;
+    @Enumerated(EnumType.STRING)
+    private VerityType verityType;
+    @OneToMany(mappedBy = "itemEntity", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<ItemSizeEntity> itemSizeEntities;
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "supplierId", nullable = false)
+    private SupplierEntity supplierEntity;
+}
+
