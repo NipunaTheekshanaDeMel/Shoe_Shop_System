df0ce32c85b91c54c0e253af8865d41578f5370e
Nipuna Theekshana <124187309+NipunaTheekshanaDeMel@users.noreply.github.com>
2024-05-31 02:19:27 +0530

    update files

--- src/main/java/software/nipunatheekshana/shoe_shop_management_system/util/Mapper.java
+++ src/main/java/software/nipunatheekshana/shoe_shop_management_system/util/Mapper.java
@@ -1,34 +1,26 @@
 package software.nipunatheekshana.shoe_shop_management_system.util;
 
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Component;
-import software.nipunatheekshana.shoe_shop_management_system.dto.CustomerDTO;
-import software.nipunatheekshana.shoe_shop_management_system.dto.EmployeeDTO;
-import software.nipunatheekshana.shoe_shop_management_system.dto.ItemDTO;
-import software.nipunatheekshana.shoe_shop_management_system.dto.SupplierDTO;
-import software.nipunatheekshana.shoe_shop_management_system.entity.CustomerEntity;
-import software.nipunatheekshana.shoe_shop_management_system.entity.EmployeeEntity;
-import software.nipunatheekshana.shoe_shop_management_system.entity.ItemEntity;
-import software.nipunatheekshana.shoe_shop_management_system.entity.SupplierEntity;
+import software.nipunatheekshana.shoe_shop_management_system.dto.*;
+import software.nipunatheekshana.shoe_shop_management_system.entity.*;
 
-
-import java.util.List;
 
 @Component
-public class Mapping {
-    private final ModelMapper mapper;
+@RequiredArgsConstructor
+public class Mapper {
 
-    Mapping(ModelMapper mapper) {
-        this.mapper = mapper;
-        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-    }
+    private final ModelMapper mapper;
 
     //Customer Mapping
     public CustomerDTO toCustomerDTO(CustomerEntity customer) {
         return  mapper.map(customer, CustomerDTO.class);
     }
-    public CustomerEntity toCustomer(CustomerDTO customerDTO) {
+    public CustomerEntity toCustomerEntity(CustomerDTO customerDTO) {
         return  mapper.map(customerDTO, CustomerEntity.class);
     }
     public List<CustomerDTO> toCustomerDTOList(List<CustomerEntity> customers) {
@@ -58,13 +50,32 @@ public class Mapping {
     }
 
     //Item Mapping
-    public ItemDTO toItemDTO(ItemEntity itemEntity) {
-        return  mapper.map(itemEntity, ItemDTO.class);
+    public ItemEntity toItemEntity(InventoryDTO inventoryDTO){
+        return mapper.map(inventoryDTO, ItemEntity.class);
     }
-    public ItemEntity toItemEntity(ItemDTO itemDTO) {
-        return  mapper.map(itemDTO, ItemEntity.class);
+
+    //User Mapping
+    public UserEntity toUserEntity(UserDTO userDTO){
+        return mapper.map(userDTO, UserEntity.class);
     }
-    public List<ItemDTO> toItemDTOList(List<ItemEntity> itemEntities) {
-        return mapper.map(itemEntities, List.class);
+    public UserDTO toUserDTO(UserEntity userEntity) {
+        return mapper.map(userEntity, UserDTO.class);
     }
+
+    //Sale Mapping
+    public SaleEntity toSaleEntity(SaleDTO saleDTO){
+        return mapper.map(saleDTO, SaleEntity.class);
+    }
+    public SaleDTO toSaleDTO(SaleEntity saleEntity){
+        return mapper.map(saleEntity, SaleDTO.class);
+    }
+    public List<SaleDTO> toSaleDTOList(List<SaleEntity> saleEntities){
+        return mapper.map(saleEntities, List.class);
+    }
+
+    //Refund Mapping
+    public RefundEntity toRefundEntity(RefundDTO refundDTO){
+        return mapper.map(refundDTO, RefundEntity.class);
+    }
+
 }
