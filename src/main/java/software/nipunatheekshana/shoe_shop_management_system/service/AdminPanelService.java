6dbd3c0708f66fd4584a06c14e541b3f8cb1dcf3
Nipuna Theekshana <124187309+NipunaTheekshanaDeMel@users.noreply.github.com>
2024-05-31 02:14:31 +0530

    add adminPanelServices

---
+++ src/main/java/software/nipunatheekshana/shoe_shop_management_system/service/AdminPanelService.java
@@ -0,0 +1,8 @@
+package software.nipunatheekshana.shoe_shop_management_system.service;
+
+import software.nipunatheekshana.shoe_shop_management_system.dto.AdminPanelDTO;
+
+
+public interface AdminPanelService {
+    AdminPanelDTO getPanelData();
+}
